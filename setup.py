b927e8320bb87c2ecc964067026d3222bfd3cc51
Jacob Neil Taylor <me@jacobtaylor.id.au>
2020-06-17 14:48:20 +1000

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plugable",
-    version="0.1.1",
+    version="0.1.9",
     author="Jacob Neil Taylor",
     author_email="me@jacobtaylor.id.au",
     description="A framework for writing extensible Python components",
