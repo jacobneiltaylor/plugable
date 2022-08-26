a5b357bbe813f84a09f750f0844f2fbdfaab86ef
Jacob Neil Taylor <me@jacobtaylor.id.au>
2022-08-26 11:13:04 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plugable",
-    version="1.0.0",
+    version="1.1.0",
     author="Jacob Neil Taylor",
     author_email="me@jacobtaylor.id.au",
     description="A framework for writing extensible Python components",
